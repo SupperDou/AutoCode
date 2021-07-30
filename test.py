ed422ed9efe631807d54813521125e26512ac79b
SupperDou <60504430+SupperDou@users.noreply.github.com>
2021-07-30 18:38:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import random,time
 USER=sys.argv[1]
 PASSWORD=sys.argv[2]
 SCKEY=sys.argv[3]
-url1 ="https://www.vikacg.xyz/wp-json/jwt-auth/v1/token"
+url1 ="https://www.vikacg.com/wp-json/jwt-auth/v1/token"
 header1 = {
 'Host': 'www.vikacg.xyz',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
@@ -13,10 +13,10 @@ header1 = {
 # 'Accept-Encoding': 'gzip, deflate, br',
 'Content-Type': 'application/x-www-form-urlencoded',
 # 'Content-Length': '149',
-'Origin': 'https://www.vikacg.xyz',
-'Alt-Used': 'www.vikacg.xyz',
+'Origin': 'https://www.vikacg.com',
+'Alt-Used': 'www.vikacg.com',
 'Connection': 'keep-alive',
-'Referer': 'https://www.vikacg.xyz/?variant=zh-cn',
+'Referer': 'https://www.vikacg.com/?variant=zh-cn',
 'Sec-Fetch-Dest': 'empty',
 'Sec-Fetch-Mode': 'cors',
 'Sec-Fetch-Site': 'same-origin'
@@ -57,10 +57,10 @@ header2 ={
 'Authorization': 'Bearer '+token,
 'Content-Type': 'application/x-www-form-urlencoded',
 'Content-Length': '16',
-'Origin': 'https://www.vikacg.xyz',
-'Alt-Used': 'www.vikacg.xyz',
+'Origin': 'https://www.vikacg.com',
+'Alt-Used': 'www.vikacg.com',
 'Connection': 'keep-alive',
-'Referer': 'https://www.vikacg.xyz/user/194980',
+# 'Referer': 'https://www.vikacg.com/user/+id',
 'Sec-Fetch-Dest': 'empty',
 'Sec-Fetch-Mode': 'cors',
 'Sec-Fetch-Site': 'same-origin',
@@ -69,17 +69,17 @@ header2 ={
 
 header3={
 
-'Host': 'www.vikacg.xyz',
+'Host': 'www.vikacg.com',
 # 'content-length': '0',
 'accept': 'application/json, text/plain, */*',
 'Authorization': 'Bearer '+token,
 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
-'origin': 'https://www.vikacg.xyz',
+'origin': 'https://www.vikacg.com',
 'x-requested-with': 'mark.via',
 'sec-fetch-site': 'same-origin',
 'sec-fetch-mode': 'cors',
 'sec-fetch-dest': 'empty',
-'referer': 'https://www.vikacg.xyz/mission/today',
+'referer': 'https://www.vikacg.com/mission/today',
 # accept-encoding: gzip, deflate
 'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
 }
@@ -93,7 +93,7 @@ data3={
 
 }
 
-url3 = "https://www.vikacg.xyz/wp-json/b2/v1/userMission"
+url3 = "https://www.vikacg.com/wp-json/b2/v1/userMission"
 
 for i in range(3):
     response1=requests.post(url3,data=data3,headers=header3)
@@ -111,24 +111,24 @@ while (num<2):
     id = random.randint(20008, 21008)
 
     header4 = {
-        'Host': 'www.vikacg.xyz',
+        'Host': 'www.vikacg.com',
         # 'content-length': '0',
         'accept': 'application/json, text/plain, */*',
         'Authorization': 'Bearer ' + token,
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
-        'origin': 'https://www.vikacg.xyz',
+        'origin': 'https://www.vikacg.com',
         'x-requested-with': 'mark.via',
         'sec-fetch-site': 'same-origin',
         'sec-fetch-mode': 'cors',
         'sec-fetch-dest': 'empty',
-        'referer': 'https://www.vikacg.xyz/user/' + str(id),
+        'referer': 'https://www.vikacg.com/user/' + str(id),
         # accept-encoding: gzip, deflate
         'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
     }
     data4 = {
         'user_id': id
     }
-    url4 = "https://www.vikacg.xyz/wp-json/b2/v1/AuthorFollow"
+    url4 = "https://www.vikacg.com/wp-json/b2/v1/AuthorFollow"
 
 
     response4 = requests.post(url4, data=data4, headers=header3)
